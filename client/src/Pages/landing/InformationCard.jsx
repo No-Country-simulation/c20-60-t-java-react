61bfdd4e94f78f0e88cf3da4ee45c9acc17a2be5
SantiagoZarate <santiago2001zarate@gmail.com>
2024-08-30 19:59:23 -0300

    style: fix information card padding

--- client/src/Pages/landing/InformationCard.jsx
+++ client/src/Pages/landing/InformationCard.jsx
@@ -3,12 +3,12 @@ import { Text } from '@/components/ui/text'
 export function InformationCard({ title, description, src, alt }) {
   return (
     <article
-      className="relative lg:flex lg:last:flex-row-reverse py-5 lg:py-0  
+      className="relative lg:flex lg:last:flex-row-reverse py-5 lg:py-0
       // 
       [&>section]:last:items-end [&>div]:last:translate-x-full"
     >
       <div className="absolute inset-0 bg-secondary top-0 -translate-x-full" />
-      <section className="flex flex-1 flex-col justify-center bg-secondary p-4">
+      <section className="flex flex-1 flex-col justify-center bg-secondary">
         <div className="flex flex-col gap-4 ">
           <Text variant="title">{title}</Text>
           <Text variant="landingCard" className="max-w-[420px]">
@@ -16,8 +16,8 @@ export function InformationCard({ title, description, src, alt }) {
           </Text>
         </div>
       </section>
-      <figure className="lg:first:mr-6 flex-1 border-0 h-72">
-        <img src={src} alt={alt} className="w-full h-full object-cover mx-auto border-0" />
+      <figure className="flex-1 h-72">
+        <img src={src} alt={alt} className="w-full h-full object-cover" />
       </figure>
     </article>
   )
