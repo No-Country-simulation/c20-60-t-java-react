26fb7401e5d8890ba6aa93df78931cbb0914ec0c
Luis Delgado <dev.ledg555@gmail.com>
2024-08-31 10:47:15 -0600

    feat: Add "choose cat or dog" component to landing page

--- client/src/Pages/landing/LandingPage.jsx
+++ client/src/Pages/landing/LandingPage.jsx
@@ -3,6 +3,7 @@ import { Hero } from '@/Pages/landing/Hero'
 import Contribute from './Contribute'
 import Stories from './Stories'
 import HowToAdopt from '@/components/common/HowToAdopt'
+import ChooseCatOrDog from '@/components/common/ChooseCatOrDog'
 import { Text } from '@/components/ui/text'
 
 const information = [
@@ -32,8 +33,20 @@ export function LandingPage() {
           return <InformationCard title={title} description={description} src={img} alt={alt} key={title} />
         })}
       </section>
-      <Text variant="display" className="text-center pb-6">¿Cómo adoptar?</Text>
-      <HowToAdopt/>
+      <Text variant="display" className="text-center pb-6">
+        ¿Cómo adoptar?
+      </Text>
+      <HowToAdopt />
+      <Text variant="display" className="text-center pt-20 pb-4">
+        Hoy mismo puedes adoptar y cambiar una vida
+      </Text>
+      <Text variant="description" className="text-center pb-9">
+        En nuestro refugio hay tanto perros como gatos esperando un hogar
+      </Text>
+      <ChooseCatOrDog />
+      <Text variant="display" className="text-center pt-20 pb-4">
+        Conoce las historias de nuestros rescatados
+      </Text>
       <Stories />
       <Contribute />
     </>
