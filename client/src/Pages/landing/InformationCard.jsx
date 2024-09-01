2f74ac182ba3fd7201cc48ac00181aca3fb2afdf
SantiagoZarate <santiago2001zarate@gmail.com>
2024-08-31 23:02:31 -0300

    style: bring back style from last commit

--- client/src/Pages/landing/InformationCard.jsx
+++ client/src/Pages/landing/InformationCard.jsx
@@ -1,9 +1,9 @@
 import { Text } from '@/components/ui/text'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
 export function InformationCard({ title, description, src, alt }) {
   return (
-    <article className="relative flex flex-col lg:flex-row lg:last:flex-row-reverse py-5 lg:py-0">
+    <article className="relative flex flex-col lg:flex-row lg:last:flex-row-reverse py-5 lg:py-0 [&>section]:last:items-end [&>div]:last:translate-x-full">
       <div className="absolute inset-0 bg-secondary top-0 -translate-x-full" />
       <section className="flex flex-1 flex-col justify-center bg-secondary p-4">
         <div className="flex flex-col gap-4">
@@ -24,5 +24,5 @@ InformationCard.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   src: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired,
-};
+  alt: PropTypes.string.isRequired
+}
