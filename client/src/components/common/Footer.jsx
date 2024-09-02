589ce16e8d3b13957911e85a9fbc296dfcbc4c8b
Luis Delgado <dev.ledg555@gmail.com>
2024-09-02 12:49:45 -0600

    fix: Improve Footer on mobile

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom'
 export default function Footer() {
   return (
     <footer className="bg-secondary ">
-      <section className="max-w-desktop mx-auto flex flex-wrap justify-between px-20 pt-8 mt-8">
-        <FooterSection title={'Navegacion'} links={['inicio', 'Quiero adoptar', 'Sobre nosotros']} />
+      <section className="max-w-desktop mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-10 sm:px-20 pt-8 mt-8">
+        <FooterSection title={'Navegación'} links={['inicio', 'Quiero adoptar', 'Sobre nosotros']} />
         <FooterSection title={'Legal'} links={['Términos legales', 'Politicas de privacidad']} />
         <FooterSection title={'Información de contacto'} links={['Calle Falsa 1234, Buenos Aires, Argentina', 'pawsomefriends@gmail.com']} />
         <FooterSection title={'Nuestras redes sociales'} links={['@PawsomeFriends', 'PawsomeFriends', '+54 11 1234-5678']} />
       </section>
-      <Text variant="medium" className="text-center my-12">
+      <Text variant="medium" className="text-center m-10">
         © 2024 Refugio Pawsome Friends. Todos los derechos reservados.
       </Text>
     </footer>
