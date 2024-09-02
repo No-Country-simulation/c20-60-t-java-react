2333db3b80160e2f891b7dc32145372f9612066a
roncalloj <roncalloj@outlook.com>
2024-09-01 21:19:12 -0500

    feat: update format of service code

--- server/src/upload_images.js
+++ server/src/upload_images.js
@@ -1,25 +1,25 @@
-import { v2 as cloudinary } from 'cloudinary';
-import 'dotenv/config';
+import { v2 as cloudinary } from 'cloudinary'
+import 'dotenv/config'
 
-(async function () {
+;(async function () {
   // Configuration
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
-  });
+  })
 
   // Upload an image
-  const path = '../server/images/';
+  const path = '../server/images/'
   const uploadResult = await cloudinary.uploader
     .upload(`${path}image.png`, {
       public_id: 'first cat'
     })
     .catch((error) => {
-      console.log(error);
-    });
+      console.log(error)
+    })
 
-  console.log(uploadResult);
+  console.log(uploadResult)
 
   // Optimize delivery by resizing and applying auto-format and auto-quality
   const optimizeUrl = cloudinary.url(uploadResult.public_id, {
@@ -35,7 +35,7 @@ import 'dotenv/config';
         height: 500
       }
     ]
-  });
+  })
 
-  console.log(optimizeUrl);
-})();
+  console.log(optimizeUrl)
+})()
