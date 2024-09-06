7337e56086e359a5babcc800aab226fa446c844f
roncalloj <roncalloj@outlook.com>
2024-09-06 18:43:17 -0500

    fix/deleting unnecessary files for swagger documentation

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ import http from 'http'
 import app from './app.js'
 
 const server = http.createServer(app)
-const PORT = process.env.SERVER_PORT || 3001
+const PORT = process.env.PORT || 3001
 
 server.listen(PORT, () => {
   console.log(`The server is all fired up on port ${PORT}`)
