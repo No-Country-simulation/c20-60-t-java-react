974e87860cbb55edf92153b0d35917ff3ed6b8d3
SantiagoZarate <santiago2001zarate@gmail.com>
2024-09-11 12:17:13 -0300

    feat: add client side pets navigation

--- client/src/Pages/pets/PetsGrid.jsx
+++ client/src/Pages/pets/PetsGrid.jsx
@@ -1,25 +1,34 @@
 import { Grid } from '@/components/ui/grid'
-import { PetCard } from './petCard'
-import { PetsPagination } from './PetsPagination'
-import { AnimatePresence } from 'framer-motion'
 import { MotionItem } from '@/components/ui/motion-item'
 import { usePetFilter } from '@/hooks/usePetFilter'
+import { AnimatePresence } from 'framer-motion'
+import { useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { PetsPagination } from './PetsPagination'
+import { PetCard } from './petCard'
+
+const PETS_PER_PAGE = 6
 
 export function PetsGrid() {
+  const [searchParams] = useSearchParams()
+  const currentPage = Number(searchParams.get('page')) || 1
   const { filteredPets } = usePetFilter()
+  const end = 0 + currentPage * PETS_PER_PAGE
+  const [paginatedPets, setPaginatedPets] = useState(filteredPets?.slice(end - PETS_PER_PAGE, end))
+  const pagesAmount = [...Array.from({ length: Math.ceil(filteredPets.length / PETS_PER_PAGE) }, (_, index) => index + 1)]
 
   return (
     <>
       <Grid>
         <AnimatePresence mode="popLayout" initial={false}>
-          {filteredPets?.map((pet) => (
+          {paginatedPets?.map((pet) => (
             <MotionItem key={pet.id}>
               <PetCard {...pet} />
             </MotionItem>
           ))}
         </AnimatePresence>
       </Grid>
-      <PetsPagination />
+      <PetsPagination activePage={currentPage} pagesAmount={pagesAmount} />
     </>
   )
 }
--- client/src/Pages/pets/PetsPagination.jsx
+++ client/src/Pages/pets/PetsPagination.jsx
@@ -1,19 +1,15 @@
 import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from '@/components/ui/pagination'
-import { useSearchParams } from 'react-router-dom'
 
-export function PetsPagination() {
-  const [searchParams] = useSearchParams()
-  const currentPage = Number(searchParams.get('page')) || 1
-
+export function PetsPagination({ pagesAmount, activePage }) {
   return (
     <Pagination>
       <PaginationContent>
         <PaginationItem>
           <PaginationPrevious href="#" />
         </PaginationItem>
-        {[1, 2, 3, 4, 5, 6].map((page) => (
+        {pagesAmount.map((page) => (
           <PaginationItem key={page}>
-            <PaginationLink isActive={currentPage === page} href="#">
+            <PaginationLink isActive={activePage === page} href={`?page=${page}`}>
               {page}
             </PaginationLink>
           </PaginationItem>
