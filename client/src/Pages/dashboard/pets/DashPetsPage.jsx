0231bc32485a7fdaa97771713e7aa64ca7406bfa
SantiagoZarate <santiago2001zarate@gmail.com>
2024-09-12 23:19:48 -0300

    feat: add peticiones page

--- client/src/Pages/dashboard/pets/DashPetsPage.jsx
+++ client/src/Pages/dashboard/pets/DashPetsPage.jsx
@@ -1,9 +1,10 @@
 import { petAPI } from '@/api'
-import { Button } from '@/components/ui'
+import { Button, Text } from '@/components/ui'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import { Toggle } from '@/components/ui/toggle'
 import { toast } from '@/components/ui/use-toast'
 import { useGetPets } from '@/hooks/useGetPets'
+import { DashboardPageLayout } from '@/layout/DashboardPageLayout'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 export function DashPetsPage() {
@@ -41,31 +42,33 @@ export function DashPetsPage() {
   }
 
   return (
-    <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Nombre</TableHead>
-          <TableHead>Especie</TableHead>
-          <TableHead>En adopción</TableHead>
-          <TableHead className="text-right">Acciones</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {pets.map((pet) => (
-          <TableRow key={pet.id}>
-            <TableCell>{pet.name}</TableCell>
-            <TableCell>{pet.species}</TableCell>
-            <TableCell>
-              <Toggle onClick={() => toggleAdoptionState(pet)} active={pet.availableForAdoption} />
-            </TableCell>
-            <TableCell className="flex justify-end gap-2">
-              <Button variant="outline">editar</Button>
-              <Button>eliminar</Button>
-            </TableCell>
+    <DashboardPageLayout title="Mascotas">
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Nombre</TableHead>
+            <TableHead>Especie</TableHead>
+            <TableHead>En adopción</TableHead>
+            <TableHead className="text-right">Acciones</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {pets.map((pet) => (
+            <TableRow key={pet.id}>
+              <TableCell>{pet.name}</TableCell>
+              <TableCell>{pet.species}</TableCell>
+              <TableCell>
+                <Toggle onClick={() => toggleAdoptionState(pet)} active={pet.availableForAdoption} />
+              </TableCell>
+              <TableCell className="flex justify-end gap-2">
+                <Button variant="outline">editar</Button>
+                <Button>eliminar</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </DashboardPageLayout>
   )
 }
--- client/src/data/constants.jsx
+++ client/src/data/constants.jsx
@@ -91,3 +91,118 @@ export const razasGato = [
   'Gato Montés Mestizo',
   'Otro'
 ]
+
+export const peticiones = [
+  {
+    date: new Date('2024-08-12T10:30:00'),
+    email: 'john.doe@example.com',
+    name: 'John Doe',
+    location: 'New York, NY',
+    phone_number: '123-456-7890',
+    message: 'I am interested in adopting a small dog. Do you have any available soon?',
+    pet: {
+      image:
+        'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
+      id: '1',
+      name: 'Rocco'
+    }
+  },
+  {
+    date: new Date('2024-08-15T14:45:00'),
+    email: 'jane.smith@example.com',
+    name: 'Jane Smith',
+    location: 'Los Angeles, CA',
+    phone_number: '987-654-3210',
+    message: 'Looking for a friendly cat to adopt. Please send me details.',
+    pet: {
+      image:
+        'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
+      id: '1',
+      name: 'Rocco'
+    }
+  },
+  {
+    date: new Date('2024-08-18T09:15:00'),
+    email: 'mike.jones@example.com',
+    name: 'Mike Jones',
+    location: 'Chicago, IL',
+    phone_number: '456-789-0123',
+    message: 'Do you have any puppies available for adoption? I would love to visit the shelter.',
+    pet: {
+      image:
+        'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
+      id: '1',
+      name: 'Rocco'
+    }
+  },
+  {
+    date: new Date('2024-08-21T12:00:00'),
+    email: 'sarah.adams@example.com',
+    name: 'Sarah Adams',
+    location: 'Austin, TX',
+    phone_number: '321-654-9870',
+    message: 'I am looking to adopt a senior dog. Could you send me more information?',
+    pet: {
+      image:
+        'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
+      id: '1',
+      name: 'Rocco'
+    }
+  },
+  {
+    date: new Date('2024-08-24T16:30:00'),
+    email: 'tom.baker@example.com',
+    name: 'Tom Baker',
+    location: 'Seattle, WA',
+    phone_number: '789-123-4567',
+    message: 'Interested in adopting a parrot. Please provide details on the available birds.',
+    pet: {
+      image:
+        'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
+      id: '1',
+      name: 'Rocco'
+    }
+  },
+  {
+    date: new Date('2024-08-27T11:45:00'),
+    email: 'lisa.carter@example.com',
+    name: 'Lisa Carter',
+    location: 'Miami, FL',
+    phone_number: '654-321-9870',
+    message: 'I am looking for a medium-sized dog to adopt. Let me know if you have any available.',
+    pet: {
+      image:
+        'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
+      id: '1',
+      name: 'Rocco'
+    }
+  },
+  {
+    date: new Date('2024-08-30T13:20:00'),
+    email: 'david.lee@example.com',
+    name: 'David Lee',
+    location: 'Denver, CO',
+    phone_number: '012-345-6789',
+    message: 'I would like to adopt a rabbit. Can you provide details about the adoption process?',
+    pet: {
+      image:
+        'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
+      id: '1',
+      name: 'Rocco'
+    }
+  },
+  {
+    date: new Date('2024-09-02T15:10:00'),
+    email: 'emily.wilson@example.com',
+    name: 'Emily Wilson',
+    location: 'San Francisco, CA',
+    phone_number: '543-210-6789',
+    message: 'I am interested in adopting a pair of cats. Do you have any available for adoption?',
+    pet: {
+      image:
+        'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
+      id: '1',
+      name: 'Rocco'
+    }
+  }
+]
